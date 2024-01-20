fe2fa4a1b2109d57138f8781d26450169d04abf1
Berk <berkpeker94@gmail.com>
2024-01-20 22:26:38 +0300

    removed unused libs and fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "react-native",
-  setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
   moduleFileExtensions: ["js", "jsx", "json", "node"],
   transformIgnorePatterns: [
     "node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation|@react-native/js-polyfills)",
